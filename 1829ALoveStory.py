f4a459be5e2cc875447ef4a774815dd0c946845d
Disha Agrawal <104905994+agrawaldisha@users.noreply.github.com>
2023-06-06 21:51:24 +0530

    Create 1829ALoveStory.py
    bruteforce

---
+++ 1829ALoveStory.py
@@ -0,0 +1,28 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+t=int(input())
+for i in range(t):
+    count=0
+    str=input()
+    if(str[0]!='c'):
+        count+=1
+    if(str[1]!='o'):
+        count+=1
+    if(str[2]!='d'):
+        count+=1
+    if(str[3]!='e'):
+        count+=1
+    if(str[4]!='f'):
+        count+=1
+    if(str[5]!='o'):
+        count+=1
+    if(str[6]!='r'):
+        count+=1
+    if(str[7]!='c'):
+        count+=1
+    if(str[8]!='e'):
+        count+=1
+    if(str[9]!='s'):
+        count+=1
+    print(count)
+    
