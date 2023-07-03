59234082bf894c2f5c293eb54f971559e916442f
Disha Agrawal <104905994+agrawaldisha@users.noreply.github.com>
2023-07-03 13:04:49 +0530

    Create Lucky.py

---
+++ Lucky.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    st=input()
+    a=int(st[0])+int(st[1])+int(st[2])
+    b=int(st[3])+int(st[4])+int(st[5])
+    if(a==b):
+        print("Yes")
+    else:
+        print("No")
