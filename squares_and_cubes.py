1dcf578b1b0afe1420d30ec215e2546e5a467b27
Disha Agrawal <104905994+agrawaldisha@users.noreply.github.com>
2023-07-12 21:45:22 +0530

    Create squares_and_cubes.py

---
+++ squares_and_cubes.py
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for(int i = 0; i < t; i++)
+		{
+		    if(1==1)
+		    int n = in.nextInt();
+		    System.out.println((int)(Math.sqrt(n)) + (int)(Math.cbrt(n)) - (int)(Math.cbrt(Math.sqrt(n))));
+		}
+	}
+}
