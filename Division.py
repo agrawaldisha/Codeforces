22991b866d3c1a727dc033cc7d1b00429573c968
Disha Agrawal <104905994+agrawaldisha@users.noreply.github.com>
2023-07-16 18:48:31 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    n=int(input())
+    if(n<1400):
+        print("Division 4")
+    elif(n>=1400 and n<1600):
+        print("Division 3")
+    elif(n>=1600 and n<1900):
+        print("Division 2")
+    else:
+        print("Division 1")
